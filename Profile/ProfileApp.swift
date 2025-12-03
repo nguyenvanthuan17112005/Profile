21dfc940bc5ca6493d2a92d482118a2e60bfab90
nguyenvanthuan17112005 <thuannv2563@ut.edu.vn>
2025-12-03 17:54:06 +0700

    Initial Commit

---
+++ Profile/ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileApp.swift
+//  Profile
+//
+//  Created by Nguyễn Văn Thuận on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
