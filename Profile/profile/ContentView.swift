00f0789f71ddead8d2848a98fce2f7a80bc26951
nguyenvanthuan17112005 <thuannv2563@ut.edu.vn>
2025-12-04 01:41:18 +0700

    first commit

--- Profile/profile/ContentView.swift
+++ Profile/profile/ContentView.swift
@@ -1,8 +1,8 @@
 //
 //  ContentView.swift
-//  Profile
+//  profile
 //
-//  Created by Nguyễn Văn Thuận on 3/12/25.
+//  Created by Nguyễn Văn Thuận on 4/12/25.
 //
 
 import SwiftUI
--- Profile/profile/profileApp.swift
+++ Profile/profile/profileApp.swift
@@ -1,14 +1,14 @@
 //
-//  ProfileApp.swift
-//  Profile
+//  profileApp.swift
+//  profile
 //
-//  Created by Nguyễn Văn Thuận on 3/12/25.
+//  Created by Nguyễn Văn Thuận on 4/12/25.
 //
 
 import SwiftUI
 
 @main
-struct ProfileApp: App {
+struct profileApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
